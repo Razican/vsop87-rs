ec8faef0eb3116eb7d96f5e0b3a0268b66114199
Razican <admin@razican.com>
2015-09-18 23:25:58 +0200

    Implemented #1

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,51 @@ fn calculate_t(jde: f64) -> f64{
 fn calculate_var(t: f64, var: &[(f64, f64, f64)]) -> f64 {
     var.iter().fold(0_f64, |term, &(a, b, c)| term + a*(b + c*t).cos())
 }
+
+/// Calculates the keplerian orbital elements from VSOP87
+/// TODO
+/// This function calculates the keplerian orbital elements from the VSOP87 solution (the
+/// heliocentric orbital elements). The parameters needed are the 6 variables returned by the
+/// VSOP87 function for a given planet. It returns, in order, a tuple with the keplerian orbital
+/// elements of the planet:
+///
+/// - Eccentricity (*e*)
+/// - Semimajor axis (*a*), in *AU*
+/// - Inclination (*i*), in radians
+/// - Longitude of the ascending node (*Ω*), in radians
+/// - Longitude of the periapsis (*ϖ*), in radians
+/// - Mean longitude (L₀), in radians
+///
+/// # Examples
+///
+/// ```
+/// use vsop87::*;
+///
+/// let (a, l, k, h, q, p) = vsop87::mercury(2451545.0);
+///
+/// # assert!(a > 0.3870982121 && a < 0.3870982123);
+/// # assert!(l > 4.4026057778 && l < 4.4026057780);
+/// # assert!(k > 0.0446647517 && k < 0.0446647519);
+/// # assert!(h > 0.2007208957 && h < 0.2007208959);
+/// # assert!(q > 0.0406161540 && q < 0.0406161542);
+/// # assert!(p > 0.04563512 && p < 0.04563588);
+///
+/// let (a, e, i, lan, lper, l) = keplerian_elements_from_vsop87(a, l, k, h, q, p);
+///
+/// assert!(a > 0.387097 && a < 0.387099);
+/// assert!(e > 0.205629 && e < 0.205631);
+/// assert!(i > 0.122260 && i < 0.122261);
+/// assert!(lan > 0.843525 && lan < 0.843527);
+/// assert!(lper >  1.35183 && lper < 1.35185);
+/// assert!(l > 4.40259 && l < 4.40261);
+/// ```
+pub fn keplerian_elements_from_vsop87(a: f64, l: f64, k: f64, h: f64, q: f64, p: f64)
+    -> (f64, f64, f64, f64, f64, f64) {
+
+    let e = (h*h + k*k).sqrt();
+    let i = (1_f64 - 2_f64*(p*p + q*q)).acos();
+    let lan = (p/q).atan();
+    let lper = (h/e).asin();
+
+    (a, e, i, lan, lper, l)
+}
--- tests/lib.rs
+++ tests/lib.rs
@@ -6,3 +6,24 @@ mod vsop87_tests;
 // mod vsop87c_tests;
 // mod vsop87d_tests;
 // mod vsop87e_tests;
+
+#[test]
+fn it_kepler() {
+    let (a, l, k, h, q, p) = vsop87::vsop87::mercury(2451545.0);
+
+    assert!(a > 0.3870982121 && a < 0.3870982123);
+    assert!(l > 4.4026057778 && l < 4.4026057780);
+    assert!(k > 0.0446647517 && k < 0.0446647519);
+    assert!(h > 0.2007208957 && h < 0.2007208959);
+    assert!(q > 0.0406161540 && q < 0.0406161542);
+    assert!(p > 0.04563512 && p < 0.04563588);
+
+    let (a, e, i, lan, lper, l) = vsop87::keplerian_elements_from_vsop87(a, l, k, h, q, p);
+
+    assert!(a > 0.387097 && a < 0.387099);
+    assert!(e > 0.205629 && e < 0.205631);
+    assert!(i > 0.122260 && i < 0.122261);
+    assert!(lan > 0.843525 && lan < 0.843527);
+    assert!(lper >  1.35183 && lper < 1.35185);
+    assert!(l > 4.40259 && l < 4.40261);
+}
